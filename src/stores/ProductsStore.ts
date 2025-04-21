3cfe7368638b6715804cb6c8bd08e7261b3ccd79
qqcrashtestqq <artemlcenko@gmail.com>
2025-04-21 14:18:13 +0300

    fix: update API URL in environment file and refactor fetchProducts to use environment variable

--- src/stores/ProductsStore.ts
+++ src/stores/ProductsStore.ts
@@ -10,11 +10,8 @@ export const useProductStore = defineStore("allProducts", {
   actions: {
     async fetchProducts() {
       try {
-        const response = await fetch(
-          "https://sneakers-api-ndi4.onrender.com/products"
-        );
+        const response = await fetch(`${import.meta.env.VITE_API_URL}products`);
         const result = await response.json();
-        // console.log("result", result);
         this.product = result.data;
       } catch (error) {
         console.error(error);
